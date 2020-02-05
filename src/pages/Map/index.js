97fb6e611ca7352a2a19e0a48b095105be2fac5f
Álvaro Oliveira <alvarogab6@gmail.com>
2020-02-05 10:25:26 -0300

    update page Map

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Alert, StyleSheet, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import uuid from 'uuid/v4';
@@ -16,7 +16,9 @@ export default class Map extends Component {
         super(props);
         this.state = {
             idRun: '',
-            press: false,
+            pressStart: false,
+            pressPause: true,
+            pressStop: false,
             coords: [],
             watch: null,
             updateMap: true,
@@ -50,57 +52,77 @@ export default class Map extends Component {
         }
     };
 
-    watchPosition = async () => {
-        const { press, watch } = this.state;
-        if (press) {
-            watch.remove();
-            this.setState({ press: false, coords: [], watch: null });
-            this.stopRun();
-        } else {
-            this.setState({ press: true });
-            await this.beginRun();
+    beginRun = async () => {
+        const { region, idRun } = this.state;
+        this.setState({ pressStart: true, pressPause: false, pressStop: true });
+        if (idRun === '') {
+            const id = uuid();
+            this.setState({
+                idRun: id,
+                coords: [{
+                    latitude: region.latitude,
+                    longitude: region.longitude
+                }]
+            });
+            await firebase.firestore().collection('runs').doc(id).set({
+                begin: new Date().getTime(),
+                end: null
+            });
+            await firebase.firestore().collection('runs').doc(id).collection('coords').add({
+                latitude: region.latitude,
+                longitude: region.longitude,
+                timestamp: new Date().getTime()
+            });
             setTimeout(() => {
                 this.setState({ updateMap: false });
                 Alert.alert('Atenção', 'Iremos parar de atualizar em tempo real.');
             }, 30000);
-            const position = await Location.watchPositionAsync({
-                accuracy: Location.Accuracy.Highest,
-                timeInterval: 2000,
-                distanceInterval: 0,
-            }, async ({ coords }) => {
-                this.savePosition(coords);
-            });
-            this.setState({ watch: position });
         }
+        this.watchPosition();
     };
 
-    beginRun = async () => {
-        const { region } = this.state;
-        const id = uuid();
-        this.setState({
-            idRun: id,
-            coords: [{
-                latitude: region.latitude,
-                longitude: region.longitude
-            }]
-        });
-        await firebase.firestore().collection('runs').doc(id).set({
-            begin: new Date().getTime(),
-            end: null
-        });
-        await firebase.firestore().collection('runs').doc(id).collection('coords').add({
-            latitude: region.latitude,
-            longitude: region.longitude,
-            timestamp: new Date().getTime()
-        });
+    pauseRun = async () => {
+        this.setState({ pressStart: false, pressPause: true });
+        const { watch, idRun } = this.state;
+        watch.remove();
+        firebase.firestore().collection('runs').doc(idRun).collection('coords').orderBy('timestamp').get()
+            .then(snapshot => {
+                const coords = [];
+                snapshot.forEach(doc => {
+                    coords.push({ latitude: doc.data().latitude, longitude: doc.data().longitude });
+                });
+                const lastCoord = coords[coords.length - 1];
+                const { latitudeDelta, longitudeDelta } = currentPosition(lastCoord.latitude, 0);
+                this.setState({
+                    watch: null, coords, region: {
+                        latitude: lastCoord.latitude,
+                        longitude: lastCoord.longitude,
+                        latitudeDelta: latitudeDelta,
+                        longitudeDelta: longitudeDelta
+                    }
+                });
+            });
     };
 
     stopRun = async () => {
-        const { idRun } = this.state;
-        await firebase.firestore().collection('runs').doc(idRun).update({
-            end: new Date().getTime()
+        this.setState({ pressStart: false, pressPause: true, pressStop: false });
+        const { idRun, watch } = this.state;
+        if (watch !== null) {
+            watch.remove();
+        }
+        await firebase.firestore().collection('runs').doc(idRun).update({ end: new Date().getTime() });
+        this.setState({ updateMap: true, idRun: '', coords: [], watch: null });
+    };
+
+    watchPosition = async () => {
+        const position = await Location.watchPositionAsync({
+            accuracy: Location.Accuracy.Highest,
+            timeInterval: 2000,
+            distanceInterval: 0,
+        }, async ({ coords }) => {
+            this.savePosition(coords);
         });
-        this.setState({ updateMap: true });
+        this.setState({ watch: position });
     };
 
     savePosition = async (coords) => {
@@ -131,7 +153,7 @@ export default class Map extends Component {
     };
 
     render() {
-        const { region, coords, press } = this.state;
+        const { region, coords } = this.state;
         return (
             <View style={styles.container}>
                 <MapView
@@ -143,20 +165,38 @@ export default class Map extends Component {
                     <Polyline coordinates={coords} strokeWidth={3} strokeColor="#4C4CFF" />
                     <Marker coordinate={region} />
                 </MapView>
-                <TouchableOpacity
-                    style={[styles.buttonFloat, { backgroundColor: !press ? '#4C4CFF' : '#B20000' }]}
-                    onPress={this.watchPosition}
-                >
+                <View style={styles.viewButtons}>
                     {
-                        !press ?
-                            (
-                                <Ionicons name="md-play" size={25} color="#FFF" />
-                            ) :
-                            (
-                                <Ionicons name="md-stopwatch" size={25} color="#FFF" />
-                            )
+                        !this.state.pressStart && (
+                            <TouchableOpacity
+                                style={[styles.buttonFloat, { backgroundColor: '#4C4CFF' }]}
+                                onPress={this.beginRun}
+                            >
+                                <Ionicons name="md-play" size={22} color="#FFF" />
+                            </TouchableOpacity>
+                        )
                     }
-                </TouchableOpacity>
+                    {
+                        !this.state.pressPause && (
+                            <TouchableOpacity
+                                style={[styles.buttonFloat, { backgroundColor: '#00B200' }]}
+                                onPress={this.pauseRun}
+                            >
+                                <FontAwesome name="pause" size={20} color="#FFF" />
+                            </TouchableOpacity>
+                        )
+                    }
+                    {
+                        this.state.pressStop && (
+                            <TouchableOpacity
+                                style={[styles.buttonFloat, { backgroundColor: '#E50000' }]}
+                                onPress={this.stopRun}
+                            >
+                                <FontAwesome name="stop" size={20} color="#FFF" />
+                            </TouchableOpacity>
+                        )
+                    }
+                </View>
             </View>
         )
     }
@@ -172,13 +212,19 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height - 70,
     },
+    viewButtons: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
     buttonFloat: {
-        position: 'absolute',
         width: 50,
         height: 50,
+        bottom: 40,
         alignItems: 'center',
         justifyContent: 'center',
-        bottom: 20,
         borderRadius: 50,
-    }
+        marginRight: 20
+    },
 });
