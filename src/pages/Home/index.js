82b717869b6ea53c47bf48a4f49e6d1b37efaa2d
Álvaro Oliveira <alvarogab6@gmail.com>
2020-02-25 13:16:54 -0300

    update page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,41 +1,23 @@
 import React, { Component } from 'react';
 import { View, ActivityIndicator, StyleSheet } from 'react-native';
-import { AppLoading } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import * as Font from 'expo-font';
 import { CommonActions } from '@react-navigation/native';
 
 export default class Home extends Component {
-    state = {
-        isReady: false
-    };
-
     async componentDidMount() {
-        /*await Font.loadAsync({
-            Roboto: require('native-base/Fonts/Roboto.ttf'),
-            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-            ...Ionicons.font,
-        });*/
-        this.setState({ isReady: true });
-        setTimeout(() => {
-            this.props.navigation.dispatch(CommonActions.reset({
-                index: 0,
-                routes: [{ name: 'Tabs' }]
-            }));
-        }, 200);
+        this.props.navigation.dispatch(CommonActions.reset({
+            index: 0,
+            routes: [{ name: 'Tabs' }]
+        }));
     }
 
     render() {
-        if (!this.state.isReady) {
-            return <AppLoading />;
-        }
         return (
             <View style={styles.container}>
                 <ActivityIndicator size="large" color="#0000ff" />
             </View>
         )
     }
-}
+};
 
 const styles = StyleSheet.create({
     container: {
