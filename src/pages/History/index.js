a99ec22a7c974ca5fada467382c23139bdc30233
Álvaro Oliveira <alvarogab6@gmail.com>
2020-02-25 13:17:55 -0300

    update page History

--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Content, Header, Left, Right, Title, List, ListItem, Body, Text, Thumbnail } from 'native-base';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import SkeletonContent from "react-native-skeleton-content";
+import { ListItem, Header } from 'react-native-elements';
 import { format } from 'date-fns';
 import '@firebase/firestore';
 
@@ -23,37 +25,92 @@ export default class History extends Component {
             });
     }
 
-    render() {
+    listRuns = () => {
         const { runs } = this.state;
         const { navigation } = this.props;
+        if (runs.length === 0) {
+            return this.skeleton();
+        } else {
+            return runs.map(data => (
+                <ListItem
+                    key={data.id}
+                    leftAvatar={{
+                        size: 55,
+                        source: require('../../../assets/map.png'),
+                        overlayContainerStyle: { backgroundColor: 'transparent' },
+                    }}
+                    title={format(data.begin, 'dd/MM/yyyy')}
+                    subtitle={
+                        <View>
+                            <Text style={styles.textOption}>
+                                Hora da Partida: {format(data.begin, 'HH:mm')}
+                            </Text>
+                            <Text style={styles.textOption}>
+                                Hora da Chegada: {format(data.end, 'HH:mm')}
+                            </Text>
+                            <Text style={styles.textOption}>
+                                Distância: {data.distance} KM
+                            </Text>
+                        </View>
+                    }
+                    onPress={() => navigation.navigate('Way', { id: data.id })}
+                />
+            ))
+        }
+    };
+
+    skeleton = () => {        
+        return Array.from({ length: 10 }).map((_, i) => (
+            <View key={i} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
+                <SkeletonContent
+                    containerStyle={{ marginLeft: 10 }}
+                    animationDirection="horizontalLeft"
+                    layout={[{ width: 70, height: 70, borderRadius: 50 }]}
+                    isLoading={true}
+                />
+                <SkeletonContent
+                    containerStyle={{ marginLeft: 5 }}
+                    animationDirection="horizontalLeft"
+                    layout={[
+                        { width: 150, height: 15, marginBottom: 5 },
+                        { width: 100, height: 10, marginBottom: 5 },
+                        { width: 100, height: 10, marginBottom: 5 },
+                        { width: 100, height: 10, marginBottom: 5 }
+                    ]}
+                    isLoading={true}
+                />
+            </View>
+        ))
+    };
+
+    render() {
         return (
-            <Container>
-                <Header style={{ backgroundColor: '#4C4CFF' }}>
-                    <Left>
-                        <Title>Histórico</Title>
-                    </Left>
-                    <Right />
-                </Header>
-                <Content>
-                    <List>
-                        {
-                            runs.map(data => (
-                                <ListItem avatar key={data.id} button={true} onPress={() => navigation.navigate('Way', { id: data.id })}>
-                                    <Left>
-                                        <Thumbnail source={require('../../../assets/map.png')} />
-                                    </Left>
-                                    <Body>
-                                        <Text>Data: {format(data.begin, 'dd/MM/yyyy')}</Text>
-                                        <Text note>Hora da Partida: {format(data.begin, 'HH:mm')}</Text>
-                                        <Text note>Hora da Chegada: {format(data.end, 'HH:mm')}</Text>
-                                        <Text note>Distância: {data.distance} KM</Text>
-                                    </Body>
-                                </ListItem>
-                            ))
-                        }
-                    </List>
-                </Content>
-            </Container>
+            <>
+                <Header
+                    placement="left"
+                    leftComponent={{ text: 'Histórico', style: styles.titleHeader }}
+                    backgroundColor="#4C4CFF"
+                />
+                <ScrollView style={styles.containerScroll}>
+                    {this.listRuns()}
+                </ScrollView>
+            </>
         )
     }
 };
+
+const styles = StyleSheet.create({
+    containerScroll: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    titleHeader: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: '600'
+    },
+    textOption: {
+        color: 'gray',
+        fontSize: 12
+    }
+});
