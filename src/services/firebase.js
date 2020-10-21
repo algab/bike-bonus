479841550d9805a553b250f3bdaa78aeb4d7e32e
Álvaro Oliveira <alvarogab6@gmail.com>
2020-10-21 14:31:10 -0300

    update service firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBdt4ve0bH535W4n3E6_EMmw4gQM3NWi84",
-    authDomain: "bike-firebase-35c3f.firebaseapp.com",
-    databaseURL: "https://bike-firebase-35c3f.firebaseio.com",
-    projectId: "bike-firebase-35c3f",
-    storageBucket: "bike-firebase-35c3f.appspot.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
 }
 
 const connection = firebase.initializeApp(config);
